781512465c2bfef099ee90fc57f0e9601fda418d
Peter van der Vlugt <Petorrr@users.noreply.github.com>
2023-01-05 20:57:08 +0100

    Update IthoCC1101.cpp

--- IthoCC1101.cpp
+++ IthoCC1101.cpp
@@ -286,8 +286,8 @@ void IthoCC1101::initReceive()
 	writeRegister(CC1101_IOCFG2 ,0x00);			//Assert when RX FIFO is filled or above the RX FIFO threshold. Deassert when (0x00): RX FIFO is drained below threshold, or (0x01): deassert when RX FIFO is empty.
 	writeRegister(CC1101_FSCTRL1 ,0x06);
 	writeRegister(CC1101_FSCTRL0 ,0x00);
-	writeRegister(CC1101_MDMCFG4 ,0x5A);                    // For newer models 0xE8 (> 2011))
-	writeRegister(CC1101_MDMCFG3 ,0x83);                    // For newer models 0x43 (> 2011))
+	writeRegister(CC1101_MDMCFG4 ,0xE8);                    // For newer models 0xE8 (> 2011))
+	writeRegister(CC1101_MDMCFG3 ,0x43);                    // For newer models 0x43 (> 2011))
 	writeRegister(CC1101_MDMCFG2 ,0x00);		//Enable digital DC blocking filter before demodulator, 2-FSK, Disable Manchester encoding/decoding, No preamble/sync
 	writeRegister(CC1101_MDMCFG1 ,0x22);		//Disable FEC
 	writeRegister(CC1101_MDMCFG0 ,0xF8);
@@ -304,7 +304,7 @@ void IthoCC1101::initReceive()
 	writeRegister(CC1101_FSCAL3 ,0xA9);
 	writeRegister(CC1101_FSCAL2 ,0x2A);
 	writeRegister(CC1101_FSCAL1 ,0x00);
-	writeRegister(CC1101_FSCAL0 ,0x11);                    // For newer models 0x1F (> 2011))
+	writeRegister(CC1101_FSCAL0 ,0x1F);                    // For newer models 0x1F (> 2011))
 	writeRegister(CC1101_FSTEST ,0x59);
 	writeRegister(CC1101_TEST2 ,0x81);
 	writeRegister(CC1101_TEST1 ,0x35);
@@ -314,7 +314,7 @@ void IthoCC1101::initReceive()
 	writeRegister(CC1101_ADDR ,0x00);
 	writeRegister(CC1101_PKTLEN ,0xFF);
 	writeRegister(CC1101_TEST0 ,0x09);
-	writeRegister(CC1101_FSCAL2 ,0x00);                    // For newer models remove this line (> 2011))
+	//writeRegister(CC1101_FSCAL2 ,0x00);                    // For newer models remove this line (> 2011))
 
 	writeCommand(CC1101_SCAL);
 
